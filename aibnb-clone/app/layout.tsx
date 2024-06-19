2bfaae66e4e226302bd3657023c064556895c563
stino-x <107426247+stino-x@users.noreply.github.com>
2024-06-19 23:17:20 +0200

    fixing layout bug

--- aibnb-clone/app/layout.tsx
+++ aibnb-clone/app/layout.tsx
@@ -1,48 +1,48 @@
-import type { Metadata } from "next";
-import { Nunito, Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Nunito, Inter } from 'next/font/google';
+import './globals.css';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; 
-import Navbar from "./components/navbar/Navbar";
-import { Modal } from "./components/modals/Modal";
-import ClientOnly from "./components/ClientOnly";
-import RegisterModal from "./components/modals/RegisterModal";
-import ToasterProvider from "./providers/ToasterProvider";
-import LoginModal from "./components/modals/LoginModal";
-import RentModal from "./components/modals/RentModal";
-import getCurrentUSer from "./actions/getCurrentUser";
-import SearchModal from "./components/modals/SearchModal";
+import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import RegisterModal from './components/modals/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal';
+import RentModal from './components/modals/RentModal';
+import getCurrentUSer from './actions/getCurrentUser';
+import SearchModal from './components/modals/SearchModal';
 
-const inter = Inter({ subsets: ["latin"] });
-const nunito = Nunito({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
+const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Airbnb",
-  description: "Airbnb clone",
+  title: 'Airbnb',
+  description: 'Airbnb clone',
 };
 
-export default async function  RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const currentuser = await getCurrentUSer()
+}) {
+  const currentuser = await getCurrentUSer();
+  
   return (
     <html lang="en">
-      {/* Clientonly component  fixes hydration error thats caused by the app folder routing somehow */}
       <body className={nunito.className}>
-      <ClientOnly>
-      <ToasterProvider />
-      <SearchModal />
-      <RegisterModal />
-      <RentModal />
-      <LoginModal />
-      <Navbar currentUser={currentuser} />
-      </ClientOnly>
-        <div className="pt-28 pb-20">
-        {children}
-        </div>
-        </body>
+        <ClientOnly>
+          <ToasterProvider>
+            <SearchModal />
+            <RegisterModal />
+            <RentModal />
+            <LoginModal />
+            <Navbar currentUser={currentuser} />
+            <div className="pt-28 pb-20">
+              {children}
+            </div>
+          </ToasterProvider>
+        </ClientOnly>
+      </body>
     </html>
   );
 }
